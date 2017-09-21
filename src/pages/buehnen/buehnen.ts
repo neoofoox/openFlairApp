c7412e2f2abe387f798d406f55ba7b470eedb5c3
neoofoox <deniskiesel@gmx.de>
2017-09-21 19:49:27 +0200

    Update Ionic Version. Make Buehnen to Timeline

--- src/pages/buehnen/buehnen.ts
+++ src/pages/buehnen/buehnen.ts
@@ -13,13 +13,17 @@ import { OpenFlairActServiceProvider } from '../../providers/open-flair-act-serv
   templateUrl: 'buehnen.html',
 })
 export class BuehnenPage {
+  //TODO: toogle Favorites.
+  //TODO: save gigs to local Storage & make option menü for manuall reloading.
   onlyFavorites: boolean = false;
   public acts: any;
   db: any;
-  seebuehne: Gig[] = [];
-  hr3: Gig[] = [];
-  freibuehne: Gig[] = [];
-  other: Gig[] = [];
+  gigs: Gig[]= [];
+  mittwoch: Gig[] = [];
+  donnerstag: Gig[] = [];
+  freitag: Gig[] = [];
+  samstag: Gig[] = [];
+  sonntag: Gig[] = [];
   favs: any;
   constructor(public navCtrl: NavController, public actService: OpenFlairActServiceProvider) {
     this.loadGigs();
@@ -32,6 +36,7 @@ export class BuehnenPage {
         let gigs = element.act.gigs;
         gigs.forEach(gig => {
           let begin = gig.begin.integer_time;
+          let end = gig.end.integer_time;
           let weekday = gig.day_name;
           let stage = gig.stage_name;
           let gigToAdd: Gig = new Gig();
@@ -39,37 +44,79 @@ export class BuehnenPage {
             && stage != undefined && artist != undefined) {
               gigToAdd.artist = artist;
               gigToAdd.begin = begin;
+              gigToAdd.end = end;
               gigToAdd.stage = stage;
-              gigToAdd.weekday = weekday
-              switch(stage){
-                case "Seebühne":
-                  this.seebuehne.push(gigToAdd);
+              gigToAdd.weekday = weekday;
+              this.gigs.push(gigToAdd);
+              switch(weekday){
+                case "Mittwoch":
+                  this.mittwoch.push(gigToAdd);
                   break;
-                case "hr3 Bühne":
-                  this.hr3.push(gigToAdd);
+                case "Donnerstag":
+                  this.donnerstag.push(gigToAdd);
                   break;
-                case "Freibühne":
-                  this.freibuehne.push(gigToAdd);
+                case "Freitag":
+                  this.freitag.push(gigToAdd);
                   break;
-                default:
-                  this.other.push(gigToAdd);
+                case "Samstag":
+                  this.samstag.push(gigToAdd);
+                  break;
+                case "Sonntag":
+                  this.sonntag.push(gigToAdd);
+                  break;
               }
             }
         });
+        this.mittwoch.sort(this.compare);
+        this.donnerstag.sort(this.compare);
+        this.freitag.sort(this.compare);
+        this.samstag.sort(this.compare);
+        this.sonntag.sort(this.compare);
       });
     });
+    
+  }
+  compare(a,b) {
+    return (a.begin > b.begin) ? 1 : ((b.begin > a.begin) ? -1 : 0);
+  }
+
+  stageColor(stage){
+    switch(stage){
+      case "Seebühne":
+        return "stageSeebuehne";
+      case "hr3 Bühne":
+        return "stageHr3";
+      case "Freibühne":
+        return "stageFreibuehne";
+      // case "E-Werk":
+      // case "Elektrogarten":
+      // case "Hofbühne":
+      // case "Innenstadt":
+      // case "Kleinkunstzelt":
+      // case "OF-Spielfeld":
+      // case "Schlossparkbühne":
+      // case "Waldbühne":
+      // case "Walkacts":
+      // case "Weinzelt":
+      
+      default:
+        return "ofred";
+    }
   }
 
 }
 
+
 class Gig {
   begin: string = "";
+  end: string = "";
   artist: string = "";
   stage: string = "";
   weekday: string = "";
 
-  public Gig(begin:string, artist:string, stage:string, weekday: string){
+  public Gig(begin:string, end:string, artist:string, stage:string, weekday: string){
     this.begin = begin;
+    this.end = end;
     this.artist = artist;
     this.stage = stage;
     this.weekday = weekday;
